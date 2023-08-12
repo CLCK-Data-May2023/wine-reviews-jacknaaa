a21df2bc1d4be07942acc0918bb50777cfbce67a
unknown <c.sathanurukwong@gmail.com>
2023-08-12 14:38:55 -0400

    Unzip file and read .CSV

--- reviews.py
+++ reviews.py
@@ -1,2 +1,14 @@
 # add your code here
+import pandas as pd
+import zipfile
 
+# Specify the path to your .zip file
+zip_file_path = "data/winemag-data-130k-v2.csv.zip"
+
+# Extract the contents of the .zip file
+with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+    csv_file_name = zip_ref.namelist()[0]
+    with zip_ref.open(csv_file_name) as csv_file:
+        df = pd.read_csv(csv_file)
+
+print(df.head())
